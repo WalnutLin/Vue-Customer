16bbc59a9daed9678c6044ca8cf5d48323d7c477
WalnutLin <1547569652@qq.com>
2020-03-25 17:43:06 +0800

    用户管理系统

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about container">
+    <h1>前端小白</h1>
+
+    <p>奥力给！！！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
